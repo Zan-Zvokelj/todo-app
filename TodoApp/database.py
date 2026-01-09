c0fac9a0bdca88712595a619f3922f7bff55d6e9
ZanZvokelj <zan.zvokelj@academiavss.si>
2026-01-09 12:50:21 +0100

    Use DATABASE_URL from environment instead of hardcoded localhost

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,10 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://postgres:freaky123@localhost/TodoApplicationDatabase"
-)
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
